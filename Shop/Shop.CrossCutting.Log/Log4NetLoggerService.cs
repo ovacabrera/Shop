0583a13ba6789ea6a7c907ceb7b3932df719b31f
Osvaldo Cabrera <ovacabrera@hotmail.com>
2019-03-10 19:57:47 -0300

    Se agregó la Opción de Logs, con una implementacion especifica usando Log4Net guardando logs en un archivo. Se optimizo la implementacion de Retrofit para leer url de API desde una variable en archivo Config. Se cambio DI de la capa de presentacion, desde el Global.Asax se hace la registracion de cada Interface que usa cada Pantalla, de forma que no necesite hacerla de forma explicita. Las capas de Modelo reciben como parametro IExternalService a usar, por lo que ahora permiten hacerle TEST inyectandole un externalSErvice con la libreria Moq. Se hicieron mejoras visuales en pantalla de Search y de Items

---
+++ Shop/Shop.CrossCutting.Log/Log4NetLoggerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using log4net;
+
+namespace Shop.CrossCutting.Log
+{
+    public class Log4NetLoggerService : ILoggerService
+    {
+        private readonly ILog _logger;
+
+        public Log4NetLoggerService(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public void Error(string message) => _logger.Error(message);
+
+        public void Error(Exception ex, string message = null) => _logger.Error(message,ex);
+
+        public void Info(string message) => _logger.Info(message);
+    }
+}
